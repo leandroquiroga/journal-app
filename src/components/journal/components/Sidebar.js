98b4dab871fa8c5adef37a559ce7dfd3485996d7
Leandro <leandroquiroga9514@gmail.com>
2022-03-07 17:35:36 -0300

    view: When logging in it will show the username

--- src/components/journal/components/Sidebar.js
+++ src/components/journal/components/Sidebar.js
@@ -1,18 +1,32 @@
 
 import React from 'react'
 import { JournaEntries } from './JournaEntries'
+import { useDispatch, useSelector } from 'react-redux';
+import { startLogout } from '../../../actions/auth';
 
 export const Sidebar = () => {
+
+  const dispatch = useDispatch();
+  const state = useSelector(state => state.auth);
+
+  console.log(state)
+
+  const handleLogout = () => {
+    dispatch(startLogout());
+  }
   return (
     <aside className='journal__sidebar flex-column px-2'>
 
       <nav className='journal__sidebar-navbar'>
         <h3 className='mt-2'>
           <i className='far fa-moon'></i>
-          <span> Leandro</span>
+          <span> {state.name}</span>
         </h3>
 
-        <button className='button button-secondary p-1 pointer mt-2'>
+        <button
+          className='button button-secondary p-1 pointer mt-2'
+          onClick={handleLogout}
+        >
           Cerrar sesión
         </button>
       </nav>
