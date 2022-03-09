32a53922122e19987d24ffce6413178879b301eb
Leandro <leandroquiroga9514@gmail.com>
2022-03-09 18:28:45 -0300

    solved: Has been a solved a problem with aUI alerts

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -30,26 +30,23 @@ export const isFormValid = (valueFrom, dispatch) => {
   return true
 
 }
-export const isFormsValid = (valueForm, dispatch) => {
-  
-  const listEmail = ['gmail.com', 'icloud.com', 'hotmail.com.ar', 'live.com.ar', 'outlook.com', 'yahoo.com.ar']
-  const { name, email, password, passwordConfirm } = valueForm;
+export const isFormsValidLogin = (dispatch, error) => {
 
-  if (validator.isEmail(email, { host_blacklist: listEmail })) {
-    dispatch(setError('El email no es valido'));
-    setTimeout(() => dispatch(removeError()), 3000);
-    return false;
-  } else if (password !== passwordConfirm) {
-    dispatch(setError('Las contraseña no coinciden'));
-    setTimeout(() => dispatch(removeError()), 3000);
-    return false;
-  } else if (!validator.isLength(password, 6, 15)) {
-    dispatch(setError('La contraseña debe tener entre 6 y 15 carcateres'));
-    setTimeout(() => dispatch(removeError()), 3000);
-    return false
-  } else if (name.trim().length === 0) {
-    dispatch(setError('El nombre es requerido'));
-    setTimeout(() => dispatch(removeError()), 3000);
-    return false;
+
+  switch (error) {
+    case 'auth/user-not-found':
+      dispatch(setError('El usuario no exite'));
+      setTimeout(() => dispatch(removeError()), 3000);
+      return;
+    case 'auth/wrong-password': 
+      dispatch(setError('La contraseña es invalida'));
+      setTimeout(() => dispatch(removeError()), 3000);
+      return;
+    case 'auth/email-already-in-use':
+      dispatch(setError('El usuario ya existe'));
+      setTimeout(() => dispatch(removeError()), 3000);
+      return;
+    default:
+      return
   }
 }
