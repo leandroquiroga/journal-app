5bd9e9ce05f6a3abbbb6ec416650a4eab6ef4908
Leandro <leandroquiroga9514@gmail.com>
2022-03-09 18:27:09 -0300

    new: Has been added a condition a that shows different components

--- src/components/journal/Journal.js
+++ src/components/journal/Journal.js
@@ -1,17 +1,26 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Notes from '../notes/Notes'
-// import { NothingSelected } from './components/NothingSelected'
+import { NothingSelected } from './components/NothingSelected'
 import { Sidebar } from './components/Sidebar'
 
 export const Journal = () => {
+
+  const state = useSelector(state => state);
+
+  const { notes } = state;
   return (
     <div className='journal__main-content'>
 
       <Sidebar />
       <main className='width-100 m-0'>
-        {/* <NothingSelected /> */}
+        
+        {
+          (notes.active === null) 
+          ? (<NothingSelected /> )
+          : (<Notes/>)
+        }
 
-        <Notes/>
       </main>
     </div>
   )
