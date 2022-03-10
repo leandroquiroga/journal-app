850e1798b26234fec9ea2ff464e6fde7fd6b66bc
Leandro <leandroquiroga9514@gmail.com>
2022-03-10 18:31:30 -0300

    modific: Has been modificated and create a new function which allows to activate the state of the note

--- src/components/journal/components/JournalEntry.js
+++ src/components/journal/components/JournalEntry.js
@@ -1,32 +1,50 @@
 
-import React from 'react'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { activeNote } from '../../../actions/notes';
+import moment from 'moment';
+import 'moment/locale/es'
 
-export const JournalEntry = () => {
+export const JournalEntry = ({ id, date, title, body, url }) => {
+
+  const dispatch = useDispatch();
+  const noteDate = moment(date);
+
+
+  // Funcion que activa el estado de las tareas
+  const handleEntryClick = () => {
+
+    dispatch(activeNote(id, {
+      date, title, body, url
+    }));
+  }
   return (
-    <div className='flex-row journal__entry pointer'>
+    <div
+      className='flex-row journal__entry pointer'
+      onClick={handleEntryClick}
+    >
 
-      <div
-        className='journal__entry-picture'
-        style={{
-          backgroundSize: 'cover',
-          backgroundImage: 'url(https://p4.wallpaperbetter.com/wallpaper/548/852/23/pink-stars-4k-nebula-wallpaper-preview.jpg)',
-        }}
-      ></div>
+      {
+        (url) &&
+            <div
+              className='journal__entry-picture'
+              style={{
+                backgroundSize: 'cover',
+                backgroundImage: `url(${url})`
+            }}
+            ></div>
+      }
 
       <div className='p-1'>
         
-        <p className='journal__entry-title'>
-          Un nuevo amanecer...
-        </p>
-        <p className='journal__entry-content'>
-          Hoy es 3 de marzo y el dia esta nubaldo
-        </p>
+        <p className='journal__entry-title'> {title} </p>
+        <p className='journal__entry-content'> {body} </p>
 
       </div>
 
       <div className='journal__entry-date-box flex-column-center'>
-        <span> Monday </span>
-        <h4>03</h4>
+        <span> {noteDate.format('dddd')} </span>
+        <h4>{ noteDate.format('DD') }</h4>
       </div>
 
     </div>
