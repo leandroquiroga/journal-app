b38752f4476f7a4242b9ef7e60b42b4a8d10cf81
Leandro <leandroquiroga9514@gmail.com>
2022-03-10 18:23:34 -0300

    new: Has been added a new types

--- src/types/types.js
+++ src/types/types.js
@@ -14,5 +14,14 @@ export const types = {
 
   //types of loading... 
   uiStartLoading: '[UI] Start Loading',
-  uiFinishLoading: '[UI] Finish Loading'
+  uiFinishLoading: '[UI] Finish Loading',
+
+  // types of note
+  notesAddNew: '[Notes] New note',
+  notesActive: '[Notes] Set active note',
+  notesLoad: '[Notes] Load notes',
+  notesUpdated: '[Notes] Update note',
+  notesFileUrl: '[Notes] Update image url',
+  notesDelete: '[Notes] Delete note',
+  notesLogoutCleaning: '[Notes] Logout Cleanig',
 }
