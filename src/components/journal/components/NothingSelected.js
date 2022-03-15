9a10a98869994425a27e8ca0f7be86e4551a17ed
Leandro <leandroquiroga9514@gmail.com>
2022-03-15 10:12:38 -0300

    create: Has been created a new function for the control sidebar

--- src/components/journal/components/NothingSelected.js
+++ src/components/journal/components/NothingSelected.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { showSidebar } from '../../../actions/ui';
 
 export const NothingSelected = () => {
-  return (
-      <section className='nothing__content flex-column-center'>
-        <p className='text-center'>
-          Selccione una tarea 
-          <br />
-          o creé una nueva
-        </p>
+  const dispatch = useDispatch();
 
-        <i className='far fa-star fa-2x'></i> 
-      </section>
+  const handleCloseSidebar = () => dispatch(showSidebar(true))
+  return (
+
+    <section
+      className='nothing__content flex-column-center pointer'
+      onClick={handleCloseSidebar}
+    >
+      <p className='text-center'>
+        Selccione una tarea 
+        <br />
+        o creé una nueva
+      </p>
+
+      <i className='far fa-star fa-2x'></i> 
+    </section>
   )
 }
