30195178145b0e0c9dca9ae9f4ee4b97f06bbdef
Leandro <leandroquiroga9514@gmail.com>
2022-03-15 10:19:35 -0300

    create: Has been created three functions that controls the changes the value form and notes

--- src/components/notes/Notes.js
+++ src/components/notes/Notes.js
@@ -1,51 +1,87 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { NotesAppBar } from './NotesAppBar'
 import { useForm } from './../../hooks/useForm';
+import { activeNote, startDeleting } from '../../actions/notes';
 
-export default function Notes() {
+export default function Notes() { 
 
-  const [valueForm, setValueForm] = useForm({
-    title: '',
-    text: '',
-  });
+  // Nos quedamos con el estado de notes para manejar
+  // el objeto active mediante el custom hooks useForm
 
-  const { title, text } = valueForm;
+  const { active:notes } = useSelector(state => state.notes);
+  const [valueForm, setValueForm, reset] = useForm(notes);
 
+  const dispatch = useDispatch();
+
+  const { title, body, id } = valueForm;
+
+  const activeId = useRef(notes.id); 
+
+  // Solamente se cambia la referencia si y solo si
+  // los id de la referecia actual es direrente al de 
+  // id del notes. 
+  useEffect(() => {
+    if (notes.id !== activeId.current) {
+      reset(notes);
+      activeId.current = notes.id
+    }
+  }, [notes, reset]);
+
+  // Cuando detecte cambion en el valueForm disparará la accion
+  // de acutalizar la nota. 
+  useEffect(() => {
+    dispatch(activeNote(valueForm.id, {...valueForm}));
+  }, [valueForm, dispatch])
+
+
+  // Elimina la nota 
+  const handleDeleteNote = () => {
+  
+    dispatch(startDeleting(id));
+  }
   return (
     <div className='notes__main-content flex-column'>
-      <NotesAppBar />
+      <NotesAppBar valueForm={ valueForm }/>
       
       <div className='notes__content flex-column'>
         <input 
+          autoFocus
           className='notes__title-input'
           name='title'
           placeholder='Escoja un titulo...'
           onChange={setValueForm}
           type='title'
           value={title}
-          autoFocus
         />
 
         <textarea
           className='notes__textarea'
-          name='text'
+          name='body'
           placeholder='¿Que paso hoy?'
           onChange={setValueForm}
           type='text'
-          value={text}
+          value={body}
         >
 
         </textarea>
       </div>
 
-      <div className='notes__images'>
-        <img
-          src='https://allegra.flowersetcfresno.com/pic/645612_full-fondos-de-pantalla-del-espacio-4k-tecnovanguardia-pack-2-0-50-fondos-de-pantalla-para-celular-y.jpg'
-          alt='universe'
-          className='notes__images-img'
-        />
-      </div>
-
+      { (notes.url) &&
+          <div className='notes__images'>
+            <img
+              src={notes.url}
+              alt='universe'
+              className='notes__images-img'
+            />
+          </div>
+      }
+      <button
+        className='button button-error pointer p-1' 
+        onClick={handleDeleteNote}
+      >
+        {([title, body].includes('')) ? 'Cancelar' : 'Eliminar'}
+      </button>
     </div>
   )
 }
